880c4bb60211247b5905413bb933c4c89b671199
Asukumar123 <98543740+Asukumar123@users.noreply.github.com>
2025-02-12 11:12:43 +0530

    Update page.tsx

--- Photonbox-Fe/src/app/page.tsx
+++ Photonbox-Fe/src/app/page.tsx
@@ -2,7 +2,7 @@ import EnergyEfficient from '@/components/home/EnergyEfficient'
 import EvolvingNeeds from '@/components/home/EvolvingNeeds'
 import Hero from '@/components/home/Hero'
 import Monitoring from '@/components/home/Monitoring'
-import Plans from '@/components/home/Plans'
+
 import Pros from '@/components/home/Pros'
 import RentalFeature from '@/components/home/RentalFeature'
 import React from 'react'
